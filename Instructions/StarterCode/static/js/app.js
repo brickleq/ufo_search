dc3215c2d8f51735bd0cb145e9e213e0e5933d52
brickleq <pblequire@gmail.com>
2019-05-31 16:40:47 -0500

    filterByDate function doesn't filter

--- Instructions/StarterCode/static/js/app.js
+++ Instructions/StarterCode/static/js/app.js
@@ -48,7 +48,52 @@ function handleSubmit() {
     d3.select("#datetime").node().value = "";
 
     // Call function to filter by selected date
-    // filterByDate(dateInput);
+    filterByDate(dateInput);
   }
 
+function clearTable(table) {
+    var firstRow = table.rows[0];
+    var tBody = table.tBodies[0].cloneNode(false);
+    tBody.appendChild(firstRow);
+    table.replaceChild(tBody, table.tBodies[0]);
+  }
+
+function filterByDate(dateInput) {
+
+    var table = document.getElementById("ufo-table");
+    clearTable(table); // Clear current table contents
+    var table = document.getElementById("ufo-table");
+    var n = 0;
+
+    // Loop through each object in tableData array:
+    for (i = 1; i < (tableData.length +1); i++) {
+        var objectData = tableData[i-1];
+        if (objectData.datetime==dateInput) {
+                n+=1;
+                // Create an empty <tr> element and add it to the nth position of the table:
+                var row = table.insertRow(n);
+                // Insert new cells (<td> elements) at the 1st - 7th positions of the new <tr> element:
+                var cell1 = row.insertCell(0); //datetime
+                var cell2 = row.insertCell(1); //city
+                var cell3 = row.insertCell(2); //state
+                var cell4 = row.insertCell(3); //country
+                var cell5 = row.insertCell(4); //shape
+                var cell6 = row.insertCell(5); //durationMinutes
+                var cell7 = row.insertCell(6); //comments
+                // Populate the new cells with data from current data.js object:
+                cell1.innerHTML = objectData.datetime;
+                cell2.innerHTML = objectData.city;
+                cell3.innerHTML = objectData.state;
+                cell4.innerHTML = objectData.country;
+                cell5.innerHTML = objectData.shape;
+                cell6.innerHTML = objectData.durationMinutes;
+                cell7.innerHTML = objectData.comments;
+        } else {
+            console.log("Object " + i - 1 + "is not a match.");
+        }
+    }
+    if (n = 0) {
+        window.alert("No data found for ${dateInput}. Check date format (MM/DD/YYYY) or contact fox.mulder@fbi.gov for assistance.");
+        }
+}  
 d3.select("#filter-btn").on("click", handleSubmit);
